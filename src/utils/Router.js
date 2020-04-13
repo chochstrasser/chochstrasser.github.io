7e3a5f5454959fe908025e8d5dcd2ede483b39f5
Chase Hochstrasser <chochstrasser@sofi.org>
2020-04-13 09:21:55 -0600

    add slash

--- src/utils/Router.js
+++ src/utils/Router.js
@@ -1,11 +1,12 @@
 import React from "react";
 import { BrowserRouter, Route, Redirect } from "react-router-dom";
 
-console.log("This is the process.env", process.env.PUBLIC_URL);
+console.log("This is the process.env", `${process.env.PUBLIC_URL}/`);
+
 // Normalize all paths to not have trailing slashes even if they
 // matched <Route> with one:
 const Router = ({ children }) => (
-  <BrowserRouter basename={process.env.PUBLIC_URL}>
+  <BrowserRouter basename={`${process.env.PUBLIC_URL}/`}>
     <Route
       render={({ location: { pathname, search, hash } }) =>
         pathname !== "/" && pathname.slice(-1) === "/" ? (
