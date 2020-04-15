d8a3c6c10b465ab660646baab084378345de55b3
Chase Hochstrasser <chochstrasser@sofi.org>
2020-04-15 08:46:56 -0600

    move back to hashrouter

--- src/App.js
+++ src/App.js
@@ -52,7 +52,7 @@ const App = () => (
       <Route path="/auth" component={() => <div>Auth file</div>} />
       <Route path="/contextApi" component={ContextApi} />
       {/* <AuthorizedRoute path="/projects" component={AuthorizedLayout} /> */}
-      {/* <Route component={() => <div>404 File Not Found</div>} /> */}
+      <Route component={() => <div>404 File Not Found</div>} />
     </Switch>
   </Router>
 );
--- src/utils/Router.js
+++ src/utils/Router.js
@@ -1,10 +1,10 @@
 import React from "react";
-import { BrowserRouter, Route, Redirect } from "react-router-dom";
+import { HashRouter, Route, Redirect } from "react-router-dom";
 
 // Normalize all paths to not have trailing slashes even if they
 // matched <Route> with one:
 const Router = ({ children }) => (
-  <BrowserRouter>
+  <HashRouter>
     <Route
       render={({ location: { pathname, search, hash } }) =>
         pathname !== "/" && pathname.slice(-1) === "/" ? (
@@ -14,7 +14,7 @@ const Router = ({ children }) => (
         )
       }
     />
-  </BrowserRouter>
+  </HashRouter>
 );
 
 export default Router;
