f6f2ed40bd56d0d5b62c92d00a04757dc8e7642a
Chase Hochstrasser <chase.hochstrasser@gmail.com>
2020-04-22 08:56:29 -0600

    add tests for trie (#12)
    Co-authored-by: Chase Hochstrasser <chochstrasser@sofi.org>

---
+++ src/trie/__test_data__/trieTestData.js
@@ -0,0 +1,3 @@
+export const TrieDataStrings = ["abc", "123"];
+
+export const TrieDataObjects = [{ id: "abc" }, { id: "123" }];
---
+++ src/trie/__tests__/trieString.test.js
@@ -0,0 +1,48 @@
+import Trie from "../../utils/trie";
+import { TrieDataStrings } from "../__test_data__/trieTestData";
+
+describe("Trie", () => {
+  it("should return a defined trie from an array of strings with a found value of two strings in array when searching empty string", () => {
+    const trie = Trie();
+    TrieDataStrings.forEach((item) => {
+      trie.add(item, item);
+    });
+    const found = trie.search("");
+    expect(trie).toBeDefined();
+    expect(found.length).toBe(2);
+    expect(found[0]).toBe("123");
+    expect(found[1]).toBe("abc");
+  });
+
+  it("should return a defined trie from an array of strings with a found value of abc when searching abc", () => {
+    const trie = Trie();
+    TrieDataStrings.forEach((item) => {
+      trie.add(item, item);
+    });
+    const found = trie.search("abc");
+    expect(trie).toBeDefined();
+    expect(found.length).toBe(1);
+    expect(found[0]).toBe("abc");
+  });
+
+  it("should return a defined trie from an array of strings with a found value of 123 when searching 12", () => {
+    const trie = Trie();
+    TrieDataStrings.forEach((item) => {
+      trie.add(item, item);
+    });
+    const found = trie.search("12");
+    expect(trie).toBeDefined();
+    expect(found.length).toBe(1);
+    expect(found[0]).toBe("123");
+  });
+
+  it("should return a defined trie from an array of strings with a found empty array when searching NAN", () => {
+    const trie = Trie();
+    TrieDataStrings.forEach((item) => {
+      trie.add(item, item);
+    });
+    const found = trie.search("NAN");
+    expect(trie).toBeDefined();
+    expect(found.length).toBe(0);
+  });
+});
