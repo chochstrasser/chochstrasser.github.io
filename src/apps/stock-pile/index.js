08dfb0db541868c8f0d3882c85454f0b85781aca
Chase Hochstrasser <chochstrasser@sofi.org>
2020-09-14 22:28:55 -0600

    add session storage

--- src/apps/stock-pile/index.js
+++ src/apps/stock-pile/index.js
@@ -1,8 +1,13 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 const FETCH_HEADERS = {
   GET: {
     method: "GET",
+    Accept: "application/json",
+    "Content-Type": "application/json",
+  },
+  POST: {
+    method: "POST",
     Accept: "application/json",
     "Content-Type": "application/json",
   },
@@ -13,9 +18,12 @@ const URLS = {
     "https://xxcfgsjs9b.execute-api.us-west-2.amazonaws.com/v1/helloworld",
 };
 
-const getStocks = async () => {
+const getStocks = async (postData) => {
   try {
-    let response = await fetch(URLS.STOCKS, FETCH_HEADERS.GET);
+    let response = await fetch(URLS.STOCKS, {
+      ...FETCH_HEADERS.POST,
+      body: JSON.stringify({ postData }),
+    });
     let data = await response.json();
     return data;
   } catch (error) {
@@ -23,20 +31,50 @@ const getStocks = async () => {
   }
 };
 
+const isAllowedToSearch = () => {
+  return sessionStorage.getItem("hotkey");
+};
+
 const StockPile = () => {
-  const [state, setState] = useState();
+  const [state, setState] = useState({ allowed: false });
+
+  const inputEl = useRef(null);
+
+  useEffect(() => {
+    const allowed = isAllowedToSearch();
+    setState({ allowed });
+  }, []);
 
   const onClick = async () => {
-    const data = await getStocks();
-    setState(data);
+    if (!isAllowedToSearch()) return;
+    const data = await getStocks({ key: state.inputValue });
+    setState((prevState) => ({ ...prevState, ...data }));
+  };
+
+  const onSubmit = () => {
+    sessionStorage.setItem("hotkey", state.inputValue);
+    setState((prevState) => ({ ...prevState, allowed: true }));
+    inputEl.current.value = "";
+  };
+
+  const handleChange = (event) => {
+    const { value } = event.target;
+    setState((prevState) => ({ ...prevState, inputValue: value }));
   };
 
   return (
     <>
       <h1>Stock Pile page</h1>
-      <input type="text" placeholder="Search a stock symbol" />
-      <button onClick={onClick} type="button">
-        Search
+      <input
+        ref={inputEl}
+        type="text"
+        onChange={handleChange}
+        placeholder={
+          state.allowed ? "Search a stock symbol" : "Enter key value"
+        }
+      />
+      <button onClick={state.allowed ? onClick : onSubmit} type="button">
+        {state.allowed ? "Search" : "Submit"}
       </button>
       {state?.stocks?.map((item) => {
         return (
