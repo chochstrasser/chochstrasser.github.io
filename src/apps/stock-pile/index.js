1ec7757ce0bc90a21f465adb7e0a3ed41028dd7c
Chase Hochstrasser <chochstrasser@sofi.org>
2020-11-16 21:23:51 -0700

    remove unused login button

--- src/apps/stock-pile/index.js
+++ src/apps/stock-pile/index.js
@@ -2,7 +2,6 @@ import { useState } from 'react';
 import Tabs from './tabs';
 import SideBar from './sideBar';
 import MainContent from './content';
-import LoginButton from '../../components/login';
 
 const StockPile = () => {
   const [tab, setTab] = useState(null);
@@ -20,7 +19,6 @@ const StockPile = () => {
   return (
     <>
       <h1>Stock Piles</h1>
-      <LoginButton />
       <Tabs setTab={handleTabChange} currentTab={tab} />
       {tab && <SideBar currentTab={tab} setSideBarLink={handleSideBarChange} />}
       <MainContent currentTab={tab} sideBarLink={sideBarLink} />
