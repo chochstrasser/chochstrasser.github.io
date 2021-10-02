a4d0fd601d8e0145958578a3781e6b70e25fb9be
Chase Hochstrasser <chase.hochstrasser@gmail.com>
2021-10-01 23:41:27 -0600

    update logout button

--- src/components/logout/index.js
+++ src/components/logout/index.js
@@ -1,6 +1,12 @@
 import { useAuth0 } from '@auth0/auth0-react';
 import styled from 'styled-components';
 
+const Flex = styled.div`
+  display: flex;
+  flex: 1;
+  justify-content: flex-end;
+`;
+
 const Button = styled.div`
   color: #ffffff;
   margin-right: 1.25rem;
@@ -10,7 +16,11 @@ const Button = styled.div`
 const LogoutButton = () => {
   const { logout } = useAuth0();
 
-  return <Button onClick={() => logout({ returnTo: window.location.origin })}>Log Out</Button>;
+  return (
+    <Flex>
+      <Button onClick={() => logout({ returnTo: window.location.origin })}>Log Out</Button>
+    </Flex>
+  );
 };
 
 export default LogoutButton;
